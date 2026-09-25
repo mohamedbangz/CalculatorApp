9b6c164fce749cdbd2fef09bc2af3d23ef89492a
Mohamed Bangura <mohamedbangz865@gmail.com>
2026-09-24 22:32:24 -0700

    create a product and order class to create a store system

--- obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc714a7d26cb35a14e2a69b04b9145ec8cde55c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7e132dcd2fc6988ffa60dc34687904e30c4b94")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
